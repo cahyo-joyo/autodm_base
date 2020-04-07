b687209f813190a8e117fd38b6d64168bc553446
cahyo-joyo <cahyoj6@gmail.com>
2020-04-07 21:29:58 +0700

    initial commit

--- app.py
+++ app.py
@@ -17,8 +17,8 @@ def start():
                 id = dms[i]['id']
 
                 if len(message) is not 0 and len(message) < 280:
-                    if "prikitiw" in message:
-                        message = message.replace("prikitiw", "")
+                    if "-cak" or "-yuk" or "-eson" in message:
+                        
                         if len(message) is not 0:
                             if dms[i]['media'] is None:
                                 print("DM will be posted")

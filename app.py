16daedbbcc71787784055667bcf2ba4789f32a7d
cahyo-joyo <cahyoj6@gmail.com>
2020-04-11 12:08:33 +0700

    initial commit

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def start():
                 id = dms[i]['id']
 
                 if len(message) is not 0 and len(message) < 280:
-                    if "prikitiw" in message:
+                    if "-cak" or "-eson" in message:
                         message = message.replace("prikitiw", "")
                         if len(message) is not 0:
                             if dms[i]['media'] is None:
